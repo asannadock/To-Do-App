7161e3f32f30fca7be91b401cbbf0a579e17b817
ASanna <71288581+asannadock@users.noreply.github.com>
2021-01-31 18:29:21 -0600

    initial commit of to-do react app

---
+++ src/TodoItem.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TodoItem = (props) => {
+    return (
+        <div className="todo-item">
+            <input 
+                type="checkbox" 
+                checked={props.completed}
+                onChange={() => props.handleChange(props.id)} 
+            />
+            <p>{props.text}</p>
+        </div>
+    )
+}
+
+export default TodoItem
